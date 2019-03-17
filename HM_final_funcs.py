932ead1457a7076b62afc751d65c8ee54c9971ff
Eyal Leibovich <eyal.leibovich@autodesk.com>
2019-03-17 16:22:31 +0200

    add functions descriptions to all functions

--- HM_final_funcs.py
+++ HM_final_funcs.py
@@ -57,9 +57,15 @@ HANGMAN_PHOTOS = {
     """
     }
 def get_secret_word():
+    """
+    get_secret_word() is used to obtain the SECRET_WORD
+    """
     return SECRET_WORD
 
 def pri(str):
+    """
+    pri(str) sets the text color to magenta, prints the given str and resets to default
+    """
     TEXT_COLOR = fg(199)
     RES = attr('reset')
 
@@ -75,8 +81,8 @@ def start_game(WORDS_FILE_PATH, WORD_INDEX):
     :type WORDS_FILE_PATH: string
     :type WORD_INDEX: string
 
-    :return:
-    :rtype:
+    :return: no returned value
+    :rtype:  no returned value
     """
 
     letters_guessed = []
@@ -102,9 +108,9 @@ def start_game(WORDS_FILE_PATH, WORD_INDEX):
 def is_valid_input(letter_guessed):
     """
     letter_guessed checks if the argument passed is a valid English letter or not.
-
     :param letter_guessed: player's char
-    :type letter_guessed: str
+    :type letter_guessed: string
+
     :return: True or False, if the player's char is in English
     :rtype: boolean
     """
@@ -116,30 +122,28 @@ def check_valid_input(letter_guessed, old_letters_guessed):
         check_valid_input checks the letter_guessed against old_letters_guessed in the following manner:
         if letter_guessed is not in old_letters_guessed, and returns True from is_valid_input(letter_guessed)
         then return True, else return False.
-
         :param letter_guessed: player's char
         :param old_letters_guessed: list of letters guessed
-        :type letter_guessed: str
+        :type letter_guessed: string
         :type old_letters_guessed: list
         :return: True or False, if the player's char is in English and not already guessed
         :rtype: boolean
         """
-
+        valid_input = False
         if (is_valid_input(letter_guessed) and (old_letters_guessed.count(letter_guessed)) < 1):
             old_letters_guessed.append(letter_guessed)
-            return True
+            valid_input = True
 
-        return False
+        return valid_input
 
 def show_hidden_word(old_letters_guessed):
     """
-    show_hidden_word recieves the secret word and the list of old letters letter_guessed
-    and returns a string with the right old letters guessed and _ instead of not guessed letters
-
+    show_hidden_word recieves a list of old letters guessed and returns a string with the right old letters guessed and _ instead of missing letters
     :param old_letters_guessed: list of letters guessed
     :type old_letters_guessed: list
-    :return: generated string, see above
-    :rtype: str
+
+    :return: generated string, of successfully guessed chars and _ instead of chars that remain unguessed
+    :rtype: string
     """
     successfully_guessed = ("_ " * len(SECRET_WORD)).split()
     for char in old_letters_guessed:
@@ -151,11 +155,10 @@ def show_hidden_word(old_letters_guessed):
 
 def check_win(old_letters_guessed):
     """
-    check_win recieves the secret word and the list of old letters letter_guessed
-    and True if the secret word was guessed or False, otherwise
-
+    check_win recieves the list of old letters guessed and will return True if the secret word was fully guessed or False, otherwise
     :param old_letters_guessed: list of letters guessed
     :type old_letters_guessed: list
+
     :return: True if SECRET_WORD was guessed
     :rtype: boolean
     """
@@ -164,8 +167,7 @@ def check_win(old_letters_guessed):
 
 def check_guess (char):
     """
-    check_guess a char and the secret word to guess.
-            check_guess will return True if the char is in the SECRET_WORD, or False otherwise.
+    check_guess recieves a charand will return True if the char is in the SECRET_WORD, or False otherwise.
     :param char: a guessed character
     :type char: string
 
@@ -178,13 +180,13 @@ def print_hangman(num_of_tries, char):
     """
     print_hangman recieves the number of tries a user has and does the following:
         if the character guessed is valild, and is part of the secret word - print the current state of HangMan, return the same amount of guesses left.
-        Else, print the HangMan pose after the unsuccessful guess, and return the num_of_tries - 1
+        Else, print the HangMan pose after the unsuccessful guess, and return the num_of_tries + 1
     :param num_of_tries: player's number of tries
-    :param SECRET_WORD: player's word to guess
+    :param char: player's guessed char
     :type num_of_tries: int
-    :type SECRET_WORD: string
+    :type char: string
 
-    :return: the number of guesses the player has left
+    :return: the number of guesses the player has accumulated
     :rtype: int
     """
 
@@ -194,10 +196,18 @@ def print_hangman(num_of_tries, char):
     return num_of_tries
 
 def get_guessed_index(successfully_guessed):
+    """
+    get_guessed_index recieves a successfully guessed char in SECRET_WORD and returns all the indexes of that char in SECRET_WORD
+    :param successfully_guessed: successfully guessed char
+    :type successfully_guessed: string
+
+    :return: list of indexes of successfully_guessed in SECRET_WORD
+    :rtype: list
+    """
     indexes = []
     i = SECRET_WORD.find(successfully_guessed)
     while (i != -1):
-        indexes.append(i) #.find(successfully_guessed, i))
+        indexes.append(i)
         i = SECRET_WORD.find(successfully_guessed, i+1)
     return indexes
 
@@ -220,7 +230,7 @@ def choose_word(file_path, index):
     f.close()
     words_set = sorted(set(words), key=words.index)
     words_limit = len(words_set)
-    if (index > len(words)):
+    if (index > len(words)): #in case the given index is bigger than the amount of words in the file
         index = index - (len(words) * (index // len(words)))
 
     words_list = list(words_set)
@@ -230,6 +240,9 @@ def choose_word(file_path, index):
     return (words_limit, words[index-1])
 
 def pri_logo():
+    """
+    pri_logo will use pri() to print out the logo of Hangman
+    """
     HANGMAN_ASCII_ART ="""
  =============================================================
 |      _    _                                                 |
